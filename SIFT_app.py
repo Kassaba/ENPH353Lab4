456765c5f6001af37980c377170325410f37849a
Andrew Kassab <andrewkassab17@gmail.com>
2020-10-01 21:59:01 -0700

    Finished code

--- SIFT_app.py
+++ SIFT_app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QImage
 from python_qt_binding import loadUi
 
 import cv2
+import numpy as np
 import sys
 
 class My_App(QtWidgets.QMainWindow):
@@ -11,15 +13,93 @@ class My_App(QtWidgets.QMainWindow):
     def __init__(self):
         super(My_App, self).__init__()
         loadUi("./SIFT_app.ui", self)
+
+        self._cam_id = 0
+        self._cam_fps = 120
+        self._is_cam_enabled = False
+        self._is_template_loaded = False
+
         self.browse_button.clicked.connect(self.SLOT_browse_button)
+        self.toggle_cam_button.clicked.connect(self.SLOT_toggle_camera)
+
+        self._camera_device = cv2.VideoCapture(self._cam_id)
+        self._camera_device.set(3, 320)
+        self._camera_device.set(4, 240)
+
+		# Timer used to trigger the camera
+        self._timer = QtCore.QTimer(self)
+        self._timer.timeout.connect(self.SLOT_query_camera)
+        self._timer.setInterval(1000 / self._cam_fps)
+
+    def convert_cv_to_pixmap(self, cv_img):
+        cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        height, width, channel = cv_img.shape
+        bytesPerLine = channel * width
+        q_img = QtGui.QImage(cv_img.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888)
+        return QtGui.QPixmap.fromImage(q_img)
+
+    def SLOT_query_camera(self):
+        ret, frame = self._camera_device.read()
+		#TODO run SIFT on the captured frame
+
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        kp_grayframe, desc_grayframe = self.sift.detectAndCompute(gray_frame, None)
+        # gray_frame = cv2.drawKeypoints(gray_frame, kp_grayframe, gray_frame)
+
+        matches = self.flann.knnMatch(self.desc_image, desc_grayframe, k = 2)
+
+        good_points = []
+        for m, n in matches:
+            if m.distance < 0.8 * n.distance:
+                good_points.append(m)
+
+        if len(good_points) > 23:
+            query_pts = np.float32([self.kp_image[m.queryIdx].pt for m in good_points]).reshape(-1, 1, 2)
+            train_pts = np.float32([kp_grayframe[m.trainIdx].pt for m in good_points]).reshape(-1, 1, 2)
+
+            matrix, mask = cv2.findHomography(query_pts, train_pts, cv2.RANSAC, 5.0)
+            matches_mask = mask.ravel().tolist()
+
+            h,w = self.init_img.shape
+            pts = np.float32([[0,0], [0,h], [w,h], [w,0]]).reshape(-1,1,2)
+            dst = cv2.perspectiveTransform(pts, matrix)
+
+            homography = cv2.polylines(frame, [np.int32(dst)], True, (255, 0, 0), 3)
+
+            pixmap = self.convert_cv_to_pixmap(homography)
+            self.live_image_label.setPixmap(pixmap)
+        else:
+            # output_image = cv2.drawMatches(self.init_img, self.kp_image, gray_frame, kp_grayframe, good_points, gray_frame)
+            pixmap = self.convert_cv_to_pixmap(frame)
+            self.live_image_label.setPixmap(pixmap)
+
+    def SLOT_toggle_camera(self):
+        if self._is_cam_enabled:
+            self._timer.stop()
+            self._is_cam_enabled = False
+            self.toggle_cam_button.setText("&Enable camera")
+            self.live_image_label.clear()
+        else:
+            self._timer.start()
+            self._is_cam_enabled = True
+            self.toggle_cam_button.setText("&Disable camera")
 
     def SLOT_browse_button(self):
         dlg = QtWidgets.QFileDialog()
         dlg.setFileMode(QtWidgets.QFileDialog.ExistingFile)
         if dlg.exec_():
-			self.template_path = dlg.selectedFiles()[0]
+            self.template_path = dlg.selectedFiles()[0]
 
-        pixmap = QtGui.QPixmap(self.template_path)
+        #Import grayscale image and find keypoints
+        self.init_img = cv2.imread(self.template_path, cv2.IMREAD_GRAYSCALE)
+        self.sift = cv2.SIFT_create()
+        self.kp_image, self.desc_image = self.sift.detectAndCompute(self.init_img, None)
+
+        index_params = dict(algorithm = 0, trees = 5)
+        search_params = dict()
+        self.flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        pixmap = QtGui.QPixmap(self.convert_cv_to_pixmap(self.init_img))
         self.template_label.setPixmap(pixmap)
 
         print("Loaded template image file: " + self.template_path)
